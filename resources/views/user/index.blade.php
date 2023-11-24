d35454830067a5a67b42c4f6b89be0770c8a43bf
try333 <triumpar@gmail.com>
2023-11-24 23:30:44 +0800

    add view user and category

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,12 +1,14 @@
+@extends('layout')
 @section('content')
-    <div class="container mx-auto mt-8">
-        <table class="min-w-full bg-white border border-gray-300">
-            <!-- Table header -->
+<div class="container mx-auto">
+    <div class="overflow-x-auto">
+        <table class="w-full md:w-2/3 lg:w-1/2 xl:w-2/5 2xl:w-1/3 bg-white border border-gray-300">
             <thead>
                 <tr>
-                    <th class="py-2 px-4 border-b">ID</th>
-                    <th class="py-2 px-4 border-b">Name</th>
-                    <th class="py-2 px-4 border-b">Email</th>
+                    <th class="py-2 px-4 border-b border-r">#</th>
+                    <th class="py-2 px-4 border-b border-r">Name</th>
+                    <th class="py-2 px-4 border-b border-r">Email</th>
+                    <th class="py-2 px-4 border-b border-r">Category</th>
                     <!-- Add other headers as needed -->
                 </tr>
             </thead>
@@ -14,9 +16,10 @@
             <tbody>
                 @foreach($data as $item)
                     <tr>
-                        <td class="py-2 px-4 border-b">{{ $item->id }}</td>
-                        <td class="py-2 px-4 border-b">{{ $item->name }}</td>
-                        <td class="py-2 px-4 border-b">{{ $item->email }}</td>
+                        <td class="py-2 px-4 border-b border-r">{{ $loop->iteration }}</td>
+                        <td class="py-2 px-4 border-b border-r">{{ $item->name }}</td>
+                        <td class="py-2 px-4 border-b border-r">{{ $item->email }}</td>
+                        <td class="py-2 px-4 border-b border-r">{{ $item->email }}</td>
                         <!-- Add other columns as needed -->
                     </tr>
                 @endforeach
@@ -26,4 +29,5 @@
         <!-- Pagination -->
         {{ $data->links() }}
     </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/user-view', [UserController::class, 'index'])->name('user-view');
