4d69d890f886e8f7110b0f2d2ca655db4721b170
n4inz <zulkar.n4inz@gmail.com>
2023-11-24 16:07:25 +0800

    add table task

--- app/Http/Controllers/api/CreateTask.php
+++ app/Http/Controllers/api/CreateTask.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Tasks;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,12 +15,37 @@ class CreateTask extends Controller
     {
         $data =  $request->all();
         $user = User::where('email' , $data['user']['user']['email'] ?? 0)->first('id');
+
+
+        if($data['category_id']){
+            $this->category($request);
+        }
+
+
         if($user){
             Tasks::updateOrCreate([
                 'user_id' => $user->id
             ],[
                 'user_id' => $user->id,
                 'task_name' => $data['task']
+            ]);
+        }
+    }
+
+
+    private static function category($request)
+    {
+        $data =  $request->all();
+        $user = User::where('email' , $data['user']['user']['email'] ?? 0)->first('id');
+
+        $category = Category::where('id', $data['category_id'])->first('id');
+        
+        if($user){
+            Tasks::updateOrCreate([
+                'user_id' => $user->id
+            ],[
+                'category_name' => $category->name,
+                'categorie_id' => $category->id
             ]);
         }
     }
