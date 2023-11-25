90c84c19521bc08990d390b0f1ae77179398a301
n4inz <zulkar.n4inz@gmail.com>
2023-11-25 16:33:42 +0800

    testing

--- app/Http/Controllers/api/CreateTask.php
+++ app/Http/Controllers/api/CreateTask.php
@@ -73,7 +73,8 @@ class CreateTask extends Controller
         $data =  $request->all();
 
         // $user = User::where('email' , $data['user']['user']['email'] ?? 0)->first('id');
-        Log::info($data['user']['action']);
+        // Log::info($data['user']['action']);
+        Log::info($data['user']);
         
         // if($user){
         //   $task =  Tasks::updateOrCreate([
