41135a2534f8dc45b0a5ca511f1ded78f71f2cf3
n4inz <zulkar.n4inz@gmail.com>
2023-11-28 07:31:11 +0800

    sds

--- app/Http/Controllers/api/CreateTask.php
+++ app/Http/Controllers/api/CreateTask.php
@@ -73,9 +73,10 @@ class CreateTask extends Controller
         $data =  $request->all();
 
         $user = User::where('email' , $data['user']['user']['email'] ?? 0)->first('id');
-        // Log::info($data['user']);
+        Log::info($data['user']);
 
-        $code = $data['user']['message']['cardsV2'][0]['card']['header']['title'];
+        // $code = $data['user']['message']['cardsV2'][0]['card']['header']['title'];
+        $code = $data['user']['message']['cards'][0]['header']['title'];
 
         $parts = explode("#", $code);
         
