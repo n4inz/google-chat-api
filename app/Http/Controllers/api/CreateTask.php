ee4630a6936d4e14f70e358eba2fb4a02835ae86
try333 <triumpar@gmail.com>
2023-12-18 17:16:03 +0800

    fix api task

--- app/Http/Controllers/api/CreateTask.php
+++ app/Http/Controllers/api/CreateTask.php
@@ -7,21 +7,18 @@ use App\Http\Resources\Task;
 use App\Models\Category;
 use App\Models\Tasks;
 use App\Models\User;
-use Dflydev\DotAccessData\Data;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class CreateTask extends Controller
 {
-    
+
     public function index(Request $request)
     {
-        $data =  $request->all();
-        $user = User::where('email' , $data['user']['user']['email'] ?? 0)->first('id');
+        $data = $request->all();
+        $user = User::where('email', $data['user']['user']['email'] ?? 0)->first('id');
 
-        
-
-        if($user){
+        if ($user) {
             // Tasks::updateOrCreate([
             //     'user_id' => $user->id
             // ],[
@@ -32,27 +29,28 @@ class CreateTask extends Controller
             $task = Tasks::create([
                 'user_id' => $user->id,
                 'task_name' => $data['task'],
-                'created_at' => now()
+                'created_at' => now(),
             ]);
-            return new Task($task);
+            return response()->json([
+                'id' => $task->id,
+            ], 200);
         }
     }
 
-
     public function category(Request $request)
     {
-        $data =  $request->all();
-        $user = User::where('email' , $data['user']['user']['email'] ?? 0)->first('id');
+        $data = $request->all();
+        $user = User::where('email', $data['user']['user']['email'] ?? 0)->first('id');
 
         $category = Category::where('name', $data['category'])->first();
 
-        if($user){
+        if ($user) {
             Tasks::updateOrCreate([
-                'user_id' => $user->id
-            ],[
+                'user_id' => $user->id,
+            ], [
                 'category_name' => $category->name,
                 'categorie_id' => $category->id,
-                'status' => 0
+                'status' => 0,
             ]);
             return new Task($task);
         }
@@ -60,15 +58,15 @@ class CreateTask extends Controller
 
     public function priority(Request $request)
     {
-        $data =  $request->all();
-        $user = User::where('email' , $data['user']['user']['email'] ?? 0)->first('id');
+        $data = $request->all();
+        $user = User::where('email', $data['user']['user']['email'] ?? 0)->first('id');
 
         $cardId = $data['user']['message']['cardsV2'][0]['cardId'];
 
-        if($user){
-          $task =  Tasks::updateOrCreate([
-                'id' => $cardId
-            ],[
+        if ($user) {
+            $task = Tasks::updateOrCreate([
+                'id' => $cardId,
+            ], [
                 'priority' => $data['priority'],
                 'ticket' => $data['ticket'],
             ]);
@@ -79,19 +77,19 @@ class CreateTask extends Controller
 
     public function choeseStatus(Request $request)
     {
-        $data =  $request->all();
+        $data = $request->all();
 
-        $user = User::where('email' , $data['user']['user']['email'] ?? 0)->first('id');
+        $user = User::where('email', $data['user']['user']['email'] ?? 0)->first('id');
         Log::info($data['user']);
         $code = $data['user']['message']['cardsV2'][0]['card']['header']['title'];
         // $code = $data['user']['message']['cards'][0]['header']['title'];
 
         $parts = explode("#", $code);
-        
-        if($user){
-          $task =  Tasks::updateOrCreate([
-                'ticket' => $parts[1]
-            ],[
+
+        if ($user) {
+            $task = Tasks::updateOrCreate([
+                'ticket' => $parts[1],
+            ], [
                 'status' => (int) $data['status'],
             ]);
             return new Task($task);
