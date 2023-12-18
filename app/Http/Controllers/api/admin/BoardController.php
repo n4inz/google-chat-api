594fb7eab3673f04cec63343b2071a76e502b9a8
try333 <triumpar@gmail.com>
2023-12-18 23:23:50 +0800

    fix board

--- app/Http/Controllers/api/admin/BoardController.php
+++ app/Http/Controllers/api/admin/BoardController.php
@@ -20,8 +20,7 @@ class BoardController extends Controller
    public function taskCount(Request $request)
    {
 
-        $taskOpen =  Tasks::where(['status', 0],
-        ['ticket', '<>', ''])->count();
+        $taskOpen =  Tasks::where('status', 0)->where_not_null('ticket')->count();
         $taskProgress =  Tasks::where('status', 1)->count();
         $taskResolved =  Tasks::where('status', 2)->count();
 
